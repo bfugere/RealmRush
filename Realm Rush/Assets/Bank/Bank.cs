d6d6fe758803fa33f0d98f2043bce498791ac5a4
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-11-05 15:00:38 -0400

    Currency Updates & UI

--- Realm Rush/Assets/Bank/Bank.cs
+++ Realm Rush/Assets/Bank/Bank.cs
@@ -1,26 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
 public class Bank : MonoBehaviour
 {
-    [SerializeField] int startingBalance = 150;
+    [SerializeField] int startingBalance = 500;
 
     [SerializeField] int currentBalance;
     public int CurrentBalance { get { return currentBalance; } }
 
+    [SerializeField] TextMeshProUGUI bankText;
+
     void Awake()
     {
         currentBalance = startingBalance;
+        UpdateBankText();
     }
 
     public void Deposit(int amount)
     {
         currentBalance += Mathf.Abs(amount);
+        UpdateBankText();
     }
 
     public void Withdraw(int amount)
     {
         currentBalance -= Mathf.Abs(amount);
+        UpdateBankText();
+
+        if (currentBalance < 0)
+        {
+            ReloadScene();
+        }
+    }
+
+    void UpdateBankText()
+    {
+        bankText.text = "Gold: " + currentBalance;
+    }
+
+    void ReloadScene()
+    {
+        Scene currentScene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene(currentScene.buildIndex);
     }
 }
