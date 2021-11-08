1151e6fe5301ffdaae3a753a7baa890cc0a57388
Bryan Fugere <45855115+bfugere@users.noreply.github.com>
2021-11-08 15:21:10 -0500

    Pathfinding Algorithm (BFS) Setup

--- Realm Rush/Assets/Tiles/Scripts/CoordinateLabeler.cs
+++ Realm Rush/Assets/Tiles/Scripts/CoordinateLabeler.cs
@@ -15,17 +15,19 @@ public class CoordinateLabeler : MonoBehaviour
 {
     [SerializeField] Color defaultColor = Color.white;
     [SerializeField] Color blockedColor = Color.yellow;
-    
+    [SerializeField] Color exploredColor = Color.blue;
+    [SerializeField] Color pathColor = new Color(1f, 0.5f, 0f); // Orange
+
     TextMeshPro label;
     Vector2Int coordinates = new Vector2Int();
-    Waypoint waypoint;
+    GridManager gridManager;
 
     void Awake()
     {
+        gridManager = FindObjectOfType<GridManager>();
         label = GetComponent<TextMeshPro>();
         label.enabled = false;
 
-        waypoint = GetComponentInParent<Waypoint>();
         DisplayCoordinates();
     }
 
@@ -50,10 +52,22 @@ public class CoordinateLabeler : MonoBehaviour
 
     void SetCoordinateLabelColor()
     {
-        if (waypoint.IsPlaceable)
-            label.color = defaultColor;
-        else
+        if (gridManager == null)
+            return;
+
+        Node node = gridManager.GetNode(coordinates);
+
+        if (node == null)
+            return;
+
+        if (!node.isWalkable)
             label.color = blockedColor;
+        else if (node.isPath)
+            label.color = pathColor;
+        else if (node.isExplored)
+            label.color = exploredColor;
+        else
+            label.color = defaultColor;
     }
 
     void DisplayCoordinates()
